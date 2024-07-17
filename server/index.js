c7a0302925014ab609dc6a3c156b8acf6a1eb5ee
Aries Dmello <dmelloaries@gmail.com>
2024-07-18 00:38:39 +0530

    backend structure

--- server/index.js
+++ server/index.js
@@ -1,21 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import userRouter from "./routes/user.routes";
+import authRouter from "./routes/auth.routes";
 dotenv.config();
 
-mongoose.connect(process.env.Mongo_URL).then(()=>{
-
+mongoose
+  .connect(process.env.Mongo_URL)
+  .then(() => {
     console.log("Database connected");
-
-}).catch((err)=>{
+  })
+  .catch((err) => {
     console.log("error in connecting");
+  });
 
-})
+const app = express();
+app.use(express.json());
 
+app.listen(3000, () => {
+  console.log("server listening on port 3000");
+});
 
-const app=express();
-
-app.listen(3000,()=>{
-    console.log("server listening on port 3000");
-
-})
+app.use("/api/user",userRouter);
+app.use("/api/auth",authRouter);
