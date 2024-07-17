1b383252ec17182d562d4eeb7e4917151db7adc5
Aries Dmello <dmelloaries@gmail.com>
2024-07-17 23:59:30 +0530

    connected MongoDB

--- server/index.js
+++ server/index.js
@@ -1,4 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+dotenv.config();
+
+mongoose.connect(process.env.Mongo_URL).then(()=>{
+
+    console.log("Database connected");
+
+}).catch((err)=>{
+    console.log("error in connecting");
+
+})
+
+
 const app=express();
 
 app.listen(3000,()=>{
