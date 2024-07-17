270b283e9f18c6e7e03d99bc4e7ddcc61b8181c1
Aries Dmello <dmelloaries@gmail.com>
2024-07-17 20:23:16 +0530

    pages added and routed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,28 @@
+import { BrowserRouter, Route,Routes } from "react-router-dom";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import SignOut from "./pages/SignOut";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile.jsx";
+
 function App() {
   
 
   return (
     <>
-      <div>
-        <h1 className="text-center">Aries</h1>
-      </div>
+      
+        <BrowserRouter >
+        <Routes>
+        <Route path="/signout" element={<SignOut/>}/>
+        <Route path="/signin" element={<Signin/>}/>
+        <Route path="/" element={<Home></Home>}/>
+        <Route path="/about" element={<About/>}/>
+       <Route path="/profile" element={<Profile/>}></Route>
+        </Routes>
+       
+        </BrowserRouter>
+      
+      
     </>
   )
 }
---
+++ client/src/pages/Signin.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Signin = () => {
+  return (
+    <div>
+        signin
+      
+    </div>
+  )
+}
+
+export default Signin
